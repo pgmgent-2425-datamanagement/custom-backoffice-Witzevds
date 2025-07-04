2632083c77c30f732a5cc554c34b3713f81e4d64
Witzevds <witzvand@student.arteveldehs.be>
2025-07-04 19:05:21 +0200

    Update project name and add EventController with initial list method

---
+++ Controllers/_EventController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+class EventController extends BaseController
+{
+
+  public static function list()
+  {
+    self::loadView('/events/list', [
+      'title' => 'Events',
+    ]);
+  }
+}
