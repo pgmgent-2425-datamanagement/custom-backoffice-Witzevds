57d45949e56fab799f7b84667e57e8092755f1ad
Witzevds <witzvand@student.arteveldehs.be>
2025-07-11 11:45:16 +0200

    Implement header layout and styling: add header structure with navigation and profile section; create main CSS and SCSS files for styling; include a CSS reset for consistent styling across browsers.

--- views/_layout/main.php
+++ views/_layout/main.php
@@ -6,22 +6,32 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= ($title ?? '') . ' ' . $_ENV['SITE_NAME'] ?></title>
-    <link rel="stylesheet" href="/css/main.css?v=<?php if ($_ENV['DEV_MODE'] == "true") {
-                                                        echo time();
-                                                    }; ?>">
+
+    <link rel="stylesheet" href="/css/reset.css">
+
+    <link rel="stylesheet" href="/css/main.css">
 </head>
 
 <body>
-    <div class="brand">BrandName</div>
+    <div class="header">
+        <nav class="header__nav">
+            <div class="nav"></div>
+            <a href="/">Home</a>
+            <a href="/events">Events</a>
+            <a href="/users">Users</a>
+            <a href="/locations">locations</a>
+            <a href="/tickets">tickets</a>
+            <a href="/dashboard">dashboard</a>
+        </nav>
+        <div class="header__profile">
+            <a href="/users/<?= $_SESSION['user_id'] ?? '' ?>">
+                <img src="/uploads/profiles/<?= $_SESSION['profile_picture'] ?? 'default.png' ?>" alt="Profile Picture">
+                <?= htmlspecialchars($_SESSION['user_name'] ?? 'Guest') ?>
+            </a>
 
-    <nav>
-        <a href="/">Home</a>
-        <a href="/events">Events</a>
-        <a href="/users">Users</a>
-        <a href="/locations">locations</a>
-        <a href="/tickets">tickets</a>
-        <a href="/dashboard">dashboard</a>
-    </nav>
+
+        </div>
+    </div>
 
     <main>
         <?= $content; ?>
